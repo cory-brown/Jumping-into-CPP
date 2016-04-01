82ea7a44dbed91447643638e16ee9fed5b8e2292
SmilinSamurai <cbrown8100@gmail.com>
2016-04-01 20:57:36 +0100

    pickchecker has changed and add a new method for picking the checkers piece

--- checkers/main.cpp
+++ checkers/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 void printBoard(int size);
 void startPositions(int size);
 void player(int index);
-int pickChecker(int playerTurn);
+void pickChecker(int playerTurn);
 
 char gameBoard[8][8];
 
-char playersChar[] = {'R','B'};
-int checkersPiece;
+char playersChar[] = {'R', 'B'};
+int columnOfPiece;
+int rowOfPiece;
 char position;
 
 enum PLAYERS {
@@ -19,11 +20,7 @@ enum PLAYERS {
 };
 
 int main() {
-    for (int j = 0; j < 8; ++j) {
-        for (int i = 0; i < 8; ++i) {
-            gameBoard[j][i] = ' '; // this is to stop the little square showing up when using the Xcode compiler
-        }
-    }
+
     cout << "Welcome to my checkers game" << endl;
     cout << "Player 1 is 'R'" << endl;
     cout << "Player 2 is 'B'" << endl;
@@ -37,6 +34,7 @@ int main() {
         printBoard(8);
         for (int i = R; i <= B; ++i) {
             player(i);
+            pickChecker(i);
             printBoard(8);
         }
     }
@@ -45,7 +43,7 @@ int main() {
 void printBoard(int size) {
     for (int i = 0; i < size; ++i) {
         for (int k = 0; k < size; ++k) {
-            cout << "---- ";
+            cout << "----- ";
         }
         cout << endl;
         for (int j = 0; j < size; ++j) {
@@ -54,7 +52,7 @@ void printBoard(int size) {
         cout << endl;
         if (i == size - 1) {
             for (int j = 0; j < size; ++j) {
-                cout << "---- ";
+                cout << "----- ";
             }
         }
     }
@@ -63,18 +61,18 @@ void printBoard(int size) {
 void startPositions(int size) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < size; ++j) {
-            if (j % 2 == 0 && i % 2 == 0) {
+            if (j % 2 == 0 && i % 2 == 0) { // odd tile space
                 gameBoard[i][j] = playersChar[0];
-            } else if (j % 2 != 0 && i % 2 != 0) {
+            } else if (j % 2 != 0 && i % 2 != 0) { // even tile space
                 gameBoard[i][j] = playersChar[0];
             }
         }
     }
     for (int i = size; i > 4; i--) {
         for (int j = size - 1; j > - 1; j--) {
-            if (j % 2 == 0 && i % 2 == 0) {
+            if (j % 2 == 0 && i % 2 == 0) {// odd tile space
                 gameBoard[i][j] = playersChar[1];
-            } else if (j % 2 != 0 && i % 2 != 0) {
+            } else if (j % 2 != 0 && i % 2 != 0) { // even tile space
                 gameBoard[i][j] = playersChar[1];
             }
         }
@@ -84,52 +82,32 @@ void startPositions(int size) {
 void player(int index) {
     cout << "\nPlayer " << index + 1 << endl;
     cout << "Which checker do you want to move" << endl;
-    cin >> checkersPiece;
+    cout << "What is the column number?" << endl;
+    cin >> columnOfPiece;
+    cout << "What is the row number?" << endl;
+    cin >> rowOfPiece;
     cout << "Left or Right" << endl;
     cout << "l/r or L/R" << endl;
     cin >> position;
 }
 
-int pickChecker(int playerTurn) {
-    int k = 0;
-    int row, column;
-    if (playerTurn == 0) {
-        for (int i = 2; i >= 0; --i) {
-            for (int j = 7; j >= 0; --j) {
-                if (gameBoard[i][j] == playersChar[0]) {
-                    k++;
-                    cout << "i " << i << endl;
-                    cout << "j " << j << endl;
-                    cout << "k " << k << endl;
-                    if (k == checkersPiece) {
-                        row = i;
-                        column = j;
-                        return gameBoard[row][column];
-                    }
+void pickChecker(int playerTurn) {
+    for (int i = 0; i < 8; ++i) { // column
+        for (int j = 0; j < 8; ++j) { // row
+            if (gameBoard[i][j] == playersChar[playerTurn]) {
+                if (i + 1 == columnOfPiece && j + 1 == rowOfPiece) { //
+
                 }
             }
         }
     }
-//    } else {
-//        for (int i = 0; i < size; ++i) {
-//            for (int j = 0; j < size; ++j) {
-//                if () {
-//                    k++;
-//                    if (k == checkersPiece) {
+}
+
+
+//void direction(int playerTurn) {
+//    if (position == 'l' || position == 'L') {
+//        pickChecker(playerTurn);
+//    } else if (position == 'r' || position == 'R') {
 //
-//                    }
-//                }
-//            }
-//        }
 //    }
-    return 0;
-}
-
-
-void direction(int playerTurn) {
-    if (position == 'l' || position == 'L') {
-        pickChecker(playerTurn);
-    } else if (position == 'r' || position == 'R') {
-
-    }
-}
+//}
